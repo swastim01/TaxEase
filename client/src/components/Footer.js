edf138d48d153603eff0e6488f009998660d47e1
swastim01 <swasti.mishra0101@gmail.com>
2025-02-25 23:32:52 +0530

    added my name in the footer and added design doc pdf

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} TaxEase. All Rights Reserved.</p>
+      <p>© {new Date().getFullYear()} TaxEase. All Rights Reserved.                  By Swasti Mishra</p>
     </footer>
   );
 }
